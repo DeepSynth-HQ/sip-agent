59cd3b1dc3b2a73c8d31256c12980d99cb3c505b
odin-hoang <uka.pgr@gmail.com>
2025-03-26 20:17:13 +0700

    fix: hide Google OAuth2 login callback endpoint

--- src/app/routes/auth.py
+++ src/app/routes/auth.py
@@ -10,7 +10,7 @@ async def google_login(request: Request):
     return await auth_handler.handle_google_login(request)
 
 
-@router.get("/callback/google")
+@router.get("/callback/google", include_in_schema=False)
 async def google_callback(code: str, state: str, request: Request):
     auth_handler = AuthHandler()
     return await auth_handler.handle_google_callback(code, state, request)
